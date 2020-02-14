c88db250d721bca3de657c8f988197e6a702509e
Julius <jubeki99@gmail.com>
2020-02-14 11:17:09 +0100

    Fix import in vue

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,6 +1,6 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('card-heading', require('./components/CardHeading'))
-    Vue.component('card-linkable', require('./components/CardLinkable'))
-    Vue.component('card-linkable-away', require('./components/CardLinkableAway'))
-    Vue.component('card-linkable-router', require('./components/CardLinkableRouter'))
+    Vue.component('card-heading', require('./components/CardHeading').default)
+    Vue.component('card-linkable', require('./components/CardLinkable').default)
+    Vue.component('card-linkable-away', require('./components/CardLinkableAway').default)
+    Vue.component('card-linkable-router', require('./components/CardLinkableRouter').default)
 })
